3cc56591d6dc4389de9995fbe0e7f7c142acdbbc
Mercy-Chemtai <chemtailodite@gmail.com>
2024-03-20 13:26:07 +0300

    practise

---
+++ src/Main.kt
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var num = 20.54F
+    println(num)
+    var d= 20.0
+    var e = 11
+    println(d+e)
+    println(d-e)
+    println(d*e)
+    println(d/e)
+
+    var width= 10
+    var length = 20
+    var area= width*length
+    println(area)
+
+    var fullName = "Mercy Chemtai"
+    var age = 20
+    val phoneNumeber = "+254011258031"
+    var weightInKg = 50.7F
+    var citizen = "Kenyan"
+    println(citizen)
+
+
+
+    }
+
+
+
